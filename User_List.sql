4e2a73379d6b17d5b2ece786715d1e69dc483bb6
Sutherland Seals <sseals1@gmail.com>
2022-11-28 17:55:24 -0600

    Added Users data and Tips data to SQL server

---
+++ User_List.sql
@@ -0,0 +1,23 @@
+﻿INSERT INTO users (name,address,email)
+VALUES ('Dion Stoade','6866 4th Court','dstoade0@cornell.edu'),
+('Windy Thorneloe','59924 Beilfuss Center','wthorneloe1@usa.gov'),
+('Hillie Phillpotts','21 Straubel Point','hphillpotts2@rakuten.co.jp'),
+('Jobye Danielou','7 Pleasure Terrace','jdanielou3@apple.com'),
+('Gabbie Schult','46273 Oak Trail','gschult4@tinyurl.com'),
+('Vasily Youdell','0993 Gulseth Avenue','vyoudell5@globo.com'),
+('Debra Blackhurst','57305 Crowley Alley','dblackhurst6@last.com'),
+('Helenelizabeth Passfield','02 Reinke Plaza','hpassfield7@netvibes.com'),
+('Franchot Slator','4930 Oneill Drive','fslator8@51.com'),
+('Renaud Erbe','30 Westridge Pass','rerbe9@psu.edu'),
+('Max Anderson','1000 infinity way Santa Ana, Costa Rica','max@email.com');
+
+     
+  
+  
+   
+  
+      
+   
+       
+
+     
