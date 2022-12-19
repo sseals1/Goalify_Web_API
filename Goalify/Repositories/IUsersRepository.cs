62488e3611eeb1a4c409124869f46aca3984b491
Sutherland Seals <sseals1@gmail.com>
2022-12-19 15:45:56 -0600

    Added the IUsers Repository to the Solution

---
+++ Goalify/Repositories/IUsersRepository.cs
@@ -0,0 +1,11 @@
+﻿using Goalify.Models;
+using System.Collections.Generic;
+
+namespace Goalify.Repositories
+{
+    public interface IUsersRepository
+    {
+        Users Get(int id);
+        List<Users> GetAll();
+    }
+}
