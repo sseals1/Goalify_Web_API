d00059d40d62e5d9165daecaddd30c6624f99176
Sutherland Seals <sseals1@gmail.com>
2023-01-16 18:10:07 -0600

    Added the Term medel class to the solution

---
+++ Goalify/Models/Terms.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Goalify.Models
+{
+    public class Terms
+    {
+        public int Id { get; set; } 
+        public string Term { get; set; }
+    }
+}
